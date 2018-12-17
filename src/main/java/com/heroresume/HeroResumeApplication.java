842753327dfbc35107065487299ae99439c94799
Brent Sullivan <brntsllvn@gmail.com>
2018-12-16 21:08:42 -0800

    init

---
+++ src/main/java/com/heroresume/HeroResumeApplication.java
@@ -0,0 +1,21 @@
+package com.heroresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class HeroResumeApplication {
+
+	public static void main(String[] args) {
+	    SpringApplication.run(HeroResumeApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+}
+
---
+++ src/test/java/com/heroresume/HeroResumeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.heroresume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeroResumeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
